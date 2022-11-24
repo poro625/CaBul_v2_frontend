65b9734cf79f3a8d21a31054ca523eb6f95f25ac
노우석 <nhowooseok@gmail.com>
2022-11-24 20:14:07 +0900

    [추가]팔로우프론트 형태구현

--- users/js/follow_api.js
+++ users/js/follow_api.js
@@ -3,20 +3,13 @@ const backEndBaseUrl = "http://127.0.0.1:8000"
 const TmdbApiImageUrl = "https://www.themoviedb.org/t/p/w220_and_h330_face"
 const TmdbApiImageOgUrl = "https://www.themoviedb.org/t/p/original/"
 
-window.onload = () => {
-    console.log('로딩되었음')
-}
-
 async function handlefollow(){
-    const follow = document.getElementById("follow").value
-    const name = document.getElementById("name").value
-    const email = document.getElementById("email").value
-    console.log(follow,name,email)
+    
 
-
-    const response = await fetch('http://127.0.0.1:8000/users/follow/', {
+    const response = await fetch(`${backEndBaseUrl}/users/follow/1/`, {
         headers: {
             'content-type': 'application/json',
+            "Authorization":"Bearer " + localStorage.getItem("access")
         },
         method: 'POST',
         body: JSON.stringify({
@@ -26,3 +19,57 @@ async function handlefollow(){
     const response_json = await response.json()
     console.log(response_json)
 }
+
+async function getuserfollow(){
+    
+
+    const response = await fetch(`${backEndBaseUrl}/users/follow/`, {
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: 'GET',
+        body: JSON.stringify({
+
+        })
+    })
+
+    // const follow_count = document.getElementById("follow.count").value
+    // const follower_count = document.getElementById("follower.count").value
+    // const user_list = document.getElementById("user_list").value
+    // const name = document.getElementById("name").value
+    // console.log(follow_count,follower_count,user_list,name)
+    const response_json = await response.json()
+    console.log(response_json)
+
+
+}
+
+window.onload = async function getfollow_API(){
+    follow_list = await getuserfollow()
+
+    var user_list =document.getElementById('user_list')
+    var wrap = document.getElementsByClassName('follow_box')[0];
+    var follow_count = document.getElementById('follow.count')
+
+
+    follow_list.forEach(follow => {
+        wrap.innerHTML += `<div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title" id="name">${follow.name}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted" id="email">${follow.email}</h6>
+                                <p class="card-text">
+                                </p>
+                                <p class="card-text" id="follow.count">
+                                    팔로잉 ${follow.follow_count}명
+                                </p>
+                                <p id = "follower.count">
+                                    팔로워 ${follow.follower_count}명
+                                </p>
+                                <button class ="card-link" onclick="handlefollow()">팔로우</button>
+                            </div>
+                        </div>`;
+    });
+        user_list.innerText = `${user_list}`
+        follow_count.innerText = `팔로우:${follow.follow_count}명` /  `팔로워:${follow.follower_count}명`
+}
+
