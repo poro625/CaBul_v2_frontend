4184ecbb152efba78b4b0bd2ad70a6782657ebf4
poro625 <poro625@naver.com>
2022-11-25 18:36:52 +0900

    [수정] 검색기능 수정

--- articles/js/search_api.js
+++ articles/js/search_api.js
@@ -1,17 +1,18 @@
 const backend_base_url = "http://127.0.0.1:8000";
 const frontend_base_url = "http://127.0.0.1:5500";
 
-//검색함수
-async function searchButton(){
-    const search_id = document.getElementById("search").value
-    
-    location.href = `search.html?${search_id}`
-    
-}
+async function getSearch(){
+    const title = document.getElementById("title").value
+    const urlObject = new URL(`${backend_base_url}/articles/search/?search=${title}`)
+    const queryString = urlObject.searchParams;
+    console.log(queryString.get('search')); // title
+    const response = await fetch(urlObject,{
+        headers : {
+            'content-type': 'application/json',
+            "Authorization":"Bearer " + localStorage.getItem("access")
 
-
-async function getSearch(search_id){
-    const response = await fetch(`${backend_base_url}/articles/search/?search=${search_id}`,{
+        },
+    
         method:'GET',
     })
     
@@ -21,36 +22,44 @@ async function getSearch(search_id){
     return response_json
     
 
-}
+ }
 
+// async function getSearch(){
+//     const response = await fetch(`${backend_base_url}/articles/search/?search=번째`,{
+//         headers : {
+//             'content-type': 'application/json',
+//             "Authorization":"Bearer " + localStorage.getItem("access")
 
-window.onload = async function loadSearch(){
-    let User_payload = JSON.parse(localStorage.getItem('payload'))
-    if (User_payload === undefined ||  User_payload === null){
+//         },
+    
+//         method:'GET',
+//     })
+    
 
-        location.href="http://127.0.0.1:5500/login.html";
-    } else {
-    const search_id = location.search.replace("?", "")
+//     response_json = await response.json()
+//     console.log(response_json)
+//     return response_json
+    
 
-    movies = await getMovieSearch(search_id)
+//  }
 
-    const movie_list = document.getElementById("movies")
+window.onload = async function getSearch_api(){
+    searchs = await getSearch()
+    
+    var search_title = document.getElementsByClassName("searchTitle")[0];
+    searchs.forEach(search => {
+        search_title.innerHTML += `
 
-    movies.forEach(movie =>{
-        const newMovie = document.createElement("div");
+        <div style="display: flex; flex-direction: row; width: 60vw; height: 50px; background-color: white; margin: 0 auto 0 40px; text-align: center; border-bottom: solid 1px #c8c4c4;">
+            <div style="width: 5vw; min-width: 50px; margin: auto;">${search.id}</div>
+            <div style="width: 5vw; min-width: 50px; margin: auto;">${search.category}</div>
+            <div style="width: 25vw; min-width: 250px; margin: auto auto auto auto; padding-left: 30px; text-align: left;"><a href="" style="color: black; text-decoration: none;"><b>${search.title}</b></a></div>
+            <div style="width: 10vw; min-width: 100px; margin: auto;">${search.user}</div>
+            <div style="width: 15vw; min-width: 150px; margin: auto;">${search.created_at}</div>
+        </div>
 
-        const movieImage = document.createElement("img")
         
-
-        movieImage.setAttribute("src", `${TmdbApiImageUrl}${movie.image}`)
-
-
-        newMovie.onclick=function() {
-            location.href = `articledetail.html?${movie.id}`
-        }
-
-        newMovie.appendChild(movieImage)
-        movie_list.appendChild(newMovie)
+        `
     });
-}
+
 }
