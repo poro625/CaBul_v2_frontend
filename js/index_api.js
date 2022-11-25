cb2d58f4e6850e329771ef00286c0a1b3427d887
hanmariyang <hanmari.code@gmail.com>
2022-11-26 02:46:02 +0900

    [수정] 게시글 상세페이지 연결

--- js/index_api.js
+++ js/index_api.js
@@ -20,6 +20,29 @@ function range(start, end) {
     return [start,...range(start + 1, end)];
     }
 
+function timeForToday(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+
+    const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+    if (betweenTime < 1) return '방금전';
+    if (betweenTime < 60) {
+        return `${betweenTime}분전`;
+    }
+
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+        return `${betweenTimeHour}시간전`;
+    }
+
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+        return `${betweenTimeDay}일전`;
+    }
+
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
+}    
+
 window.onload = async function getIndex_API(){
     let User_payload = JSON.parse(localStorage.getItem('payload'))
     
@@ -32,7 +55,7 @@ window.onload = async function getIndex_API(){
     else {
         page_id = page_number.replace("?page=", "")
     }
-    console.log(`page_id : ${page_id}`)
+    // console.log(`page_id : ${page_id}`)
     feed_list = await getIndexFeedList(page_id)
     feed_list = feed_list.articles
     // console.log(feed_list)
@@ -79,11 +102,23 @@ window.onload = async function getIndex_API(){
     var wrap = document.getElementsByClassName('FeedBoxCont')[0];
 
     feed_list.results.forEach(feed => {
+        console.log(feed)
+        console.log(`
+            pk : ${feed.pk}
+            user : ${feed.user}
+            like_count : ${feed.like_count}
+            title : ${feed.title}
+            category : ${feed.category}
+            content : ${feed.content}
+            transfer_image : ${feed.transfer_image}
+            updated_at : ${feed.updated_at}
+            user_id : ${feed.user_id}
+        `)
         wrap.innerHTML += `<div class="FeedBox" style="background-color: #fafafa; border: solid 1px #aaaaaa; box-shadow: 1px 1px 1px 1px #aaaaaa;">
                                 <div style="width: 300px; min-width: 300px; height: 400px; min-height: 400px;">
                                     <div style="display: flex; flex-direction: row; justify-content: space-between; height: 40px;"><div style="display: flex; flex-direction: row;">
                                         <img src="/static/img/default.png" alt="" style="width: 20px; height: 20px; border-radius: 10px; margin: 10px 5px 0 5px;">
-                                        <div style="font-weight: bold; margin-top: 7px ;">
+                                        <div onclick="location.href='${frontEndBaseUrl}/users/profile.html?id=${feed.user_id}'" style="font-weight: bold; margin-top: 7px ;">
                                         ${feed.user}
                                     </div>
                                             <a href= "" style="border: solid 1px #aaaaaa; border-radius: 4px; height: 25px; margin: 5px 0 0 10px; padding-left: 5px; padding-right: 5px; font-size: 11pt; text-decoration: none; color: #fafafa;; background-color: #aaaaaa;">팔로잉</a>
@@ -101,11 +136,11 @@ window.onload = async function getIndex_API(){
                                 </div>
 
                                     <div style="width: 300px; min-width: 300px; height: 280px; min-height: 280px;">
-                                        <img onclick="location.href='/articles/detail.html?id=${feed.id}'" style="cursor: pointer; width: 300px; min-width: 300px; height: 280px; min-height: 280px; object-fit: cover;" src="${feed.transfer_image}"></div>
+                                        <img onclick="location.href='/articles/detail.html?id=${feed.pk}'" style="cursor: pointer; width: 300px; min-width: 300px; height: 280px; min-height: 280px; object-fit: cover;" src="${feed.transfer_image}"></div>
                                     <div style="display: flex; flex-direction: row; justify-content: space-between;"><div style="font-size: 12pt; font-weight: bold; margin: 5px 0 0 10px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap; width: 180px;">${feed.title}</div>
                                     <form action='' method='post'>
                                         <div style="display: flex; flex-direction: row;">
-                                            <div style="margin: 10px 5px 0 0; font-size: 10pt;">좋아요n개
+                                            <div style="margin: 10px 5px 0 0; font-size: 10pt;">${feed.like_count}개
                                             </div>
                                             <!--post.like_authors.all에 user가 있다면 아래 if문 돌기-->
                                             <button style="border: none; background: none; margin-top: 3px;"><img onclick="" style="width: 20px; height: 20px; margin: 5px 10px 0 0;" src="/static/icon/heart_bk.png" /></button>
@@ -122,7 +157,7 @@ window.onload = async function getIndex_API(){
                                         </a>
                                             <br>
                                         </div>
-                                        <div style="text-align: right; margin-right: 10px; font-size: 10pt; color: #aaaaaa;">n분전</div>
+                                        <div style="text-align: right; margin-right: 10px; font-size: 10pt; color: #aaaaaa;">${timeForToday(feed.updated_at)}</div>
                                     </div>
                                 </div>
                             </div>`
@@ -130,6 +165,7 @@ window.onload = async function getIndex_API(){
         
     });
 
+    console.log(nav_user_info)
     // nav 부분
     // nav 상단 유저 박스 부분
     var nav_nickname = document.getElementsByClassName('NavUserInfoBoxNickname')[0];
@@ -138,19 +174,20 @@ window.onload = async function getIndex_API(){
     var nav_follow = document.getElementsByClassName('NavUserInfoBoxFollow')[0];
     var nav_login = document.getElementsByClassName('NavUserInfoBoxLogin')[0];
     last_login_time = timeForToday(nav_user_info.last_login)
+    // var nav_profile_link = document.getElementsByClassName('NavUserInfoBoxProfileLink')[0];
 
     nav_nickname.innerText = `${nav_user_info.nickname}`
     nav_name.innerText = `${nav_user_info.name}`
     nav_email.innerText = `${nav_user_info.email}`
     nav_follow.innerText = `팔로잉 ${nav_user_info.follow_count} 명  |  팔로워 ${nav_user_info.followee_count} 명`
     nav_login.innerText = `현재 접속 시간 : ${last_login_time}`
-
+    // nav_profile_link.setAttribute("onclick", `${frontEndBaseUrl}/users/profile.html?id=${nav_user_info.id}`)
 
     // nav 하단 카테고리 부분
     var nav_category = document.getElementsByClassName('NavCategory')[0];
     
 
     nav_category_box.forEach(category => {
-        nav_category.innerHTML += `<div class="category"><a href='' style="color: #cacaca; text-decoration: none;">${category.category} <b style="font-weight: normal; color: #cacaca;">(${category.count})</b></a></div>`
+        nav_category.innerHTML += `<div onclick="location.href='${frontEndBaseUrl}/articles/category.html?category=${category.category}'" class="category"><a style="color: #cacaca; text-decoration: none;">${category.category} <b style="font-weight: normal; color: #cacaca;">(${category.count})</b></a></div>`
     });
 }
