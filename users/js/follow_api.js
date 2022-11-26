abaaac7f7f81fa8d6a4817de5c368e344f670240
hanmariyang <hanmari.code@gmail.com>
2022-11-26 19:41:28 +0900

    [삭제] 중복 코드 제거(getUser)

--- users/js/follow_api.js
+++ users/js/follow_api.js
@@ -111,20 +111,7 @@ async function getUserFollow(){
     return response_json
 }
 
-async function getUser(){
 
-    let User_payload = JSON.parse(localStorage.getItem('payload'))
-    const response = await fetch(`${backEndBaseUrl}/users/${User_payload.user_id}/`, {
-        headers: {
-            'content-type': 'application/json',
-            "Authorization":"Bearer " + localStorage.getItem("access")
-        },
-        method: 'GET',
-    })
-
-    const response_json = await response.json()
-    return response_json
-}
 // 팔로우 페이지 출력
 window.onload = async function getFollow_API(){
     let User_payload = JSON.parse(localStorage.getItem('payload'))
